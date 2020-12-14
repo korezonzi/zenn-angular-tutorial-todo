859ac812bfc6e6a255e926e77598c8fbd5e9ef6b
korezonzi <korezonzip@gmail.com>
2020-12-15 01:40:31 +0900

    done section 02. create todo(separate components)

---
+++ src/app/task-form/task-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {Task} from '../../modules/task';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.scss']
+})
+export class TaskFormComponent implements OnInit {
+
+  constructor() { }
+  /*親コンポーネントに対して、イベントを受け渡せる*/
+  @Output() addTask = new EventEmitter<Task>();
+
+  newTask = {
+    title: '',
+    deadline: null,
+  };
+
+  ngOnInit(): void {
+  }
+  /*
+    {title~}というオブジェクトをイベントにパラメータとして添付する
+    添付したパラメータは親コンポーネント側で$eventとして受け取れる
+    */
+  submit(): void {
+    this.addTask.emit({
+      title: this.newTask.title,
+      done: false,
+      deadline: this.newTask.deadline ? new Date(this.newTask.deadline) : null,
+    });
+    this.newTask = {
+      title: '',
+      deadline: null
+    };
+  }
+
+}

036024aad868cd12decb4d3b01c1b5b1e93bfcf9
korezonzi <korezonzip@gmail.com>
2020-12-31 01:37:35 +0900

    fix memory leak bug

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Task} from '../../modules/task';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
-export class TaskListComponent implements OnInit {
+export class TaskListComponent implements OnInit, OnDestroy {
 
   constructor(
     private firestore: AngularFirestore,
   ) { }
   tasks: Task[] = [];
+  subscription: Subscription;
   addTask(task: Task): void {
     this.firestore.collection('tasks').add(task);
   }
@@ -21,12 +23,15 @@ export class TaskListComponent implements OnInit {
   /*ngOnInit:コンポーネントが初期化された直後に実行される。
   * subscribe: 変更が検知されるたびに実行*/
   ngOnInit(): void {
-    this.firestore.collection('tasks').valueChanges().subscribe((tasks: any) => {
+    this.subscription = this.firestore.collection('tasks').valueChanges().subscribe((tasks: any) => {
       this.tasks = tasks.map(task => {
         task.deadline = task.deadline ? task.deadline.toDate() : null;
         return task;
       }) as Task[];
     });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
